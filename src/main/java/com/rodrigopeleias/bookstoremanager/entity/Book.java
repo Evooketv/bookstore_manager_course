56b3c26a45b153c2e9e433fd6a64f919c337d5de
Evooketv <marcellors@live.com>
2024-09-17 15:14:24 -0300

    Developed project entities

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/entity/Book.java
@@ -0,0 +1,40 @@
+package com.rodrigopeleias.bookstoremanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "publish name", nullable = false, unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST,CascadeType.MERGE, CascadeType.REMOVE} )
+    @JoinColumn
+    private Author author;
+
+}
